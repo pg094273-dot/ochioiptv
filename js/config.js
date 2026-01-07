03276ecade73a35273f88ec10ef9b3830bdfcc6c
pg094273-dot <pg094273@gmail.com>
2026-01-07 11:42:36 +0100

    Update TMDB API key in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    TMDB_API_KEY: 'TU_API_KEY_AQUI',
+    TMDB_API_KEY: '207426834b40af0ecdfc767abc440b3f',
     TMDB_BASE_URL: 'https://api.themoviedb.org/3',
     TMDB_IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500',
     STORAGE_KEYS: {
